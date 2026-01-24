bc3ee689f8c813c0f7506a28480ef25ae8be29f4
gawrbot <rdieberger@gmail.com>
2026-01-24 11:44:35 +0100

    Add real dark/light mode

--- app/src/main/java/dev/rhea/babyalter/MainActivity.kt
+++ app/src/main/java/dev/rhea/babyalter/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.datastore.preferences.preferencesDataStore
 import dev.rhea.babyalter.data.BIRTH_DATE_KEY
@@ -15,6 +18,7 @@ import kotlinx.coroutines.flow.map
 import java.time.LocalDate
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import dev.rhea.babyalter.ui.theme.BabyalterTheme
 
 val ComponentActivity.dataStore by preferencesDataStore(name = "baby_prefs")
 
@@ -33,13 +37,20 @@ class MainActivity : ComponentActivity() {
                 .map { it[BIRTH_DATE_CALC_KEY]?.let(LocalDate::parse) }
                 .collectAsState(initial = null)
 
-            Scaffold { padding ->
-                BabyalterScreen(
-                    birthDate = birthDate,
-                    birthDateCalc = birthDateCalc,
-                    activity = this,
-                    modifier = Modifier.padding(padding)
-                )
+            BabyalterTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold { padding ->
+                        BabyalterScreen(
+                            birthDate = birthDate,
+                            birthDateCalc = birthDateCalc,
+                            activity = this,
+                            modifier = Modifier.padding(padding)
+                        )
+                    }
+                }
             }
         }
     }
--- app/src/main/java/dev/rhea/babyalter/ui/theme/Theme.kt
+++ app/src/main/java/dev/rhea/babyalter/ui/theme/Theme.kt
@@ -9,34 +9,37 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+
+    background = Color(0xFF121212),
+    surface = Color(0xFF121212),
+    onBackground = Color.White,
+    onSurface = Color.White
 )
+
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Color.White,
+    surface = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black
 )
+
 
 @Composable
 fun BabyalterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
